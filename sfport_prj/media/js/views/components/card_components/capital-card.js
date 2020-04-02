6539563509927bdc02299bed27f918aa96262af2
Rob Gaston <rgaston@fargeo.com>
2020-04-02 16:18:49 -0700

    fixes parent tile save calls, re: #17

--- sfport_prj/media/js/views/components/card_components/capital-card.js
+++ sfport_prj/media/js/views/components/card_components/capital-card.js
@@ -15,7 +15,9 @@ define([
             this.createParentAndChild = function (parenttile, childcard) {
                 if (parenttile.tileid === "") {
                     var callback = function(){childcard.selected(true);}
-                    self.form.saveTile(parenttile, callback);
+                    parenttile.save(function() {
+                        return;
+                    }, callback);
                 }
             };
 
